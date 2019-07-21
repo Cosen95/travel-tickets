1a4b4ac44400468bbf3a335a101346f689fa6262
fengshuan <1263215592@qq.com>
2019-07-21 16:17:48 +0800

    feat: React.lazy、Suspense及错误边界（Error boundaries）

--- README.md
+++ README.md
@@ -15,3 +15,62 @@ const MyContext = React.createContext(defaultValue);
 React 组件也可以订阅到 context 变更。这能让你在函数式组件中完成订阅 context。这个函数接收当前的 context 值，返回一个 React 节点。
 * Class.contextType
 挂载在 class 上的 contextType 属性会被重赋值为一个由 React.createContext() 创建的 Context 对象。这能让你使用 this.context 来消费最近 Context 上的那个值。
+
+### React.lazy、Suspense及错误边界（Error boundaries）
+* React.lazy
+```
+const OtherComponent = React.lazy(() => import('./OtherComponent'));
+
+function MyComponent() {
+  return (
+    <div>
+      <OtherComponent />
+    </div>
+  );
+}
+```
+* Suspense
+> 如果在 MyComponent 渲染完成后，包含 OtherComponent 的模块还没有被加载完成，我们可以使用加载指示器为此组件做优雅降级。这里我们使用 Suspense 组件来解决。
+```
+const OtherComponent = React.lazy(() => import('./OtherComponent'));
+
+function MyComponent() {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <OtherComponent />
+      </Suspense>
+    </div>
+  );
+}
+```
+fallback 属性接受任何在组件加载过程中你想展示的 React 元素。
+* 错误边界（Error boundaries）
+> 如果一个 class 组件中定义了 static getDerivedStateFromError() 或 componentDidCatch() 这两个生命周期方法中的任意一个（或两个）时，那么它就变成一个错误边界。当抛出错误后，请使用 static getDerivedStateFromError() 渲染备用 UI ，使用 componentDidCatch() 打印错误信息。
+```
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // 更新 state 使下一次渲染能够显示降级后的 UI
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // 你同样可以将错误日志上报给服务器
+    logErrorToMyService(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // 你可以自定义降级后的 UI 并渲染
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import React, { Component, createContext } from 'react';
+import React, { Component, createContext, lazy, Suspense } from 'react';
 import './App.css';
 
-const BatteryContext = createContext(30);
+const About = lazy(() => import(/* webpackChunkName: "about" */'./About.jsx'));
 
-class Leaf extends Component {
-  static contextType = BatteryContext;
-  render() {
-    const battery = this.context;
-    return (
-      // <BatteryContext.Consumer>
-      //   {
-      //     battery => <h1>Battery: {battery}</h1>
-      //   }
-      // </BatteryContext.Consumer>
-      <h1>Battery: {battery}</h1>
-    )
-  }
-}
+// ErrorBoundary
 
-class Middle extends Component {
-  render() {
-    return <Leaf />
-  }
-}
+// const BatteryContext = createContext(30);
+
+// class Leaf extends Component {
+//   // static contextType = BatteryContext;
+//   render() {
+//     // const battery = this.context;
+//     return (
+//       // <BatteryContext.Consumer>
+//       //   {
+//       //     battery => <h1>Battery: {battery}</h1>
+//       //   }
+//       // </BatteryContext.Consumer>
+//       // <h1>Battery: {battery}</h1>
+//     )
+//   }
+// }
+
+// class Middle extends Component {
+//   render() {
+//     return <Leaf />
+//   }
+// }
 
 class App extends Component {
+  state = {
+    hasError: false
+  }
+  // componentDidCatch() {
+  //   this.setState({
+  //     hasError: true
+  //   })
+  // }
+  static getDerivedStateFromError(){
+    return {
+      hasError: true
+    }
+  }
   render() {
+    const { hasError } = this.state;
+    if(hasError) {
+      return <div>报错了！！</div>
+    }
     return (
-      <BatteryContext.Provider value={60}>
-        <Middle />
-      </BatteryContext.Provider>
+      <div>
+        <Suspense fallback={<div>loading</div>}>
+          <About></About>
+        </Suspense>
+      </div>
+      // <BatteryContext.Provider value={60}>
+      //   <Middle />
+      // </BatteryContext.Provider>
     );
   }
 }
