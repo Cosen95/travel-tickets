8b852ef73ed32a7e75a0043bcb706c8dee274f3c
fengshuan <1263215592@qq.com>
2019-08-18 17:12:24 +0800

    feat: 日期选择浮层-日历组件

--- src/components/DateSelector.jsx
+++ src/components/DateSelector.jsx
@@ -4,12 +4,93 @@ import PropTypes from "prop-types";
 import Header from "./Header";
 import "./DateSelector.css";
 
+function Month(props) {
+  const { startingTimeInMonth, onSelect } = props;
+  const startDay = new Date(startingTimeInMonth);
+  const currentDay = new Date(startingTimeInMonth);
+
+  let days = [];
+
+  while (currentDay.getMonth() === startDay.getMonth()) {
+    days.push(currentDay.getTime());
+    currentDay.setDate(currentDay.getDate() + 1);
+  }
+
+  days = new Array(startDay.getDay() ? startDay.getDay() - 1 : 6)
+    .fill(null)
+    .concat(days);
+
+  const lastDay = new Date(days[days.length - 1]);
+
+  days = days.concat(
+    new Array(lastDay.getDay() ? 7 - lastDay.getDay() : 0).fill(null)
+  );
+
+  const weeks = [];
+
+  for (let row = 0; row < days.length / 7; ++row) {
+    const week = days.slice(row * 7, (row + 1) * 7);
+    weeks.push(week);
+  }
+  return (
+    <table className="date-table">
+      <thead>
+        <tr>
+          <td colSpan="7">
+            <h5>
+              {startDay.getFullYear()}年{startDay.getMonth() + 1}月
+            </h5>
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="data-table-weeks">
+          <th>周一</th>
+          <th>周二</th>
+          <th>周三</th>
+          <th>周四</th>
+          <th>周五</th>
+          <th className="weekend">周六</th>
+          <th className="weekend">周日</th>
+        </tr>
+        {/* {weeks.map((week, idx) => {
+              return <Week key={idx} days={week} onSelect={onSelect} />;
+          })} */}
+      </tbody>
+    </table>
+  );
+}
+
 export default function DateSelector(props) {
   const { show, onSelect, onBack } = props;
+
+  const now = new Date();
+  now.setHours(0);
+  now.setMinutes(0);
+  now.setSeconds(0);
+  now.setMilliseconds(0);
+  now.setDate(1);
+  const monthSequence = [now.getTime()];
+
+  now.setMonth(now.getMonth() + 1);
+  monthSequence.push(now.getTime());
+
+  now.setMonth(now.getMonth() + 1);
+  monthSequence.push(now.getTime());
   return (
     <div className={classnames("date-selector", { hidden: !show })}>
       <Header title="日期" onBack={onBack} />
-      <div className="date-selector-tables" />
+      <div className="date-selector-tables">
+        {monthSequence.map(month => {
+          return (
+            <Month
+              key={month}
+              onSelect={onSelect}
+              startingTimeInMonth={month}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
